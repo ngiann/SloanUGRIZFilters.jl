19e1568924d5e33d4a2910036e30d45f20514dbe
ngiann <ngiann@gmail.com>
2023-08-04 13:44:40 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SloanUGRIZFilters
 
 [![Build Status](https://github.com/ngiann/SloanUGRIZFilters.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/ngiann/SloanUGRIZFilters.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+# Methods
+
+Exports methods `filter_u`, `filter_g`, `filter_r`, `filter_i`, `filter_z` that linearly interpolate the SDSS discretised filters.
--- src/SloanUGRIZFilters.jl
+++ src/SloanUGRIZFilters.jl
@@ -1,5 +1,24 @@
 module SloanUGRIZFilters
 
-# Write your package code here.
+    using DelimitedFiles, Interpolations
+
+
+    function instantiate_filter(filename)
+
+        local data = readdlm(joinpath(joinpath(dirname(pathof(SloanUGRIZFilters)), "filters"), filename))
+
+        extrap = linear_interpolation(data[:,1], data[:,2], extrapolation_bc = 0)
+
+        x -> extrap(x)
+
+    end
+
+    filter_u = instantiate_filter("SLOAN_SDSS.u.dat")
+    filter_g = instantiate_filter("SLOAN_SDSS.g.dat")
+    filter_r = instantiate_filter("SLOAN_SDSS.r.dat")
+    filter_i = instantiate_filter("SLOAN_SDSS.i.dat")
+    filter_z = instantiate_filter("SLOAN_SDSS.z.dat")
+
+    export filter_u, filter_g, filter_r, filter_i, filter_z
 
 end
