5cd1c4aefc1244cf4e9eef016fb8e9a11e7f6459
ngiann <ngiann@gmail.com>
2023-08-04 14:02:38 +0200

    normalise filters - again

--- src/SloanUGRIZFilters.jl
+++ src/SloanUGRIZFilters.jl
@@ -1,6 +1,6 @@
 module SloanUGRIZFilters
 
-    using DelimitedFiles, Interpolations
+    using DelimitedFiles, Interpolations, QuadGK
 
 
     function instantiate_filter(filename)
@@ -9,14 +9,21 @@ module SloanUGRIZFilters
 
         extrap = linear_interpolation(data[:,1], data[:,2], extrapolation_bc = 0)
 
-        x -> extrap(x)
+        Z = quadgk(extrap, 0, 11000.00)[1]
+
+        x -> extrap(x) / Z # normalised filter❗
 
     end
 
+
     filter_u = instantiate_filter("SLOAN_SDSS.u.dat")
+    
     filter_g = instantiate_filter("SLOAN_SDSS.g.dat")
+    
     filter_r = instantiate_filter("SLOAN_SDSS.r.dat")
+    
     filter_i = instantiate_filter("SLOAN_SDSS.i.dat")
+
     filter_z = instantiate_filter("SLOAN_SDSS.z.dat")
 
     export filter_u, filter_g, filter_r, filter_i, filter_z
